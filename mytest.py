093075d81afdae85847892e2f7cfa2fc8f51e474
Double-Sha <maoshasix@163.com>
2020-11-28 16:48:11 +0800

    增加mytest.py文件

---
+++ mytest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/28 16:45
+# @Team : Private
+# @Author : shasha.mao
+# @Email : maoshasix@163.com
+# @File : mytest.py
+# @Project : frontend
+# @Tool : PyCharm
